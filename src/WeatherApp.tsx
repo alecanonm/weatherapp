2cb335ce1e4bf2e61d056d4007e25014e887d1eb
Alejandro Canon Munoz <al3jandrocan0n@gmail.com>
2026-03-12 21:57:31 -0500

    Feature: Header.tsx, components, images

--- src/WeatherApp.tsx
+++ src/WeatherApp.tsx
@@ -1,5 +1,11 @@
+import CustomHeader from "./components/Header/CustomHeader";
+
 const WeatherApp = () => {
-  return <div></div>;
+  return (
+    <div className="bg-neutral-900 min-h-screen pt-4 px-4 pb-12 xl:pt-12 xl:px-28 xl:pb-20">
+      <CustomHeader />
+    </div>
+  );
 };
 
 export default WeatherApp;
---
+++ src/components/Header/CustomHeader.tsx
@@ -0,0 +1,27 @@
+import logo from "../../assets/images/logo.svg";
+import unitsIcon from "../../assets/images/icon-units.svg";
+import dropDownIcon from "../../assets/images/icon-dropdown.svg";
+
+const CustomHeader = () => {
+  return (
+    <header className="flex justify-between items-center text-neutral-0 font-dm-sans">
+      <img
+        src={logo}
+        alt="logo"
+      />
+      <button className="flex xl:gap-2.5 gap-1.5 bg-neutral-800 py-2 px-2.5 xl:py-2 xl:px-4 rounded-lg cursor-pointer">
+        <img
+          src={unitsIcon}
+          alt="units"
+        />
+        <span>Units</span>
+        <img
+          src={dropDownIcon}
+          alt="drop down"
+        />
+      </button>
+    </header>
+  );
+};
+
+export default CustomHeader;
